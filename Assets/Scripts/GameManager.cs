b97cd52cebafe326c05cb01908f510bd66f0e3b5
Braden Walters <vc@braden-walters.info>
2019-05-04 18:49:19 +0200

    * EventSystem and GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public const string HealthChangeEvent = "GameManager.HealthChange";
+
+    private const int MaxHealth = 3;
+
+    public static GameManager Singleton { get; private set; }
+
+    void Awake()
+    {
+        Singleton = this;
+        this.TotalHealth = MaxHealth;
+        this.CurrentHealth = MaxHealth;
+    }
+
+    void OnDestroy()
+    {
+        Singleton = null;
+    }
+
+    public int TotalHealth { get; private set; }
+
+    public int CurrentHealth { get; private set; }
+
+    public void DamageSlave()
+    {
+        --this.CurrentHealth;
+        EventSystem.Publish(this, HealthChangeEvent);
+    }
+}
