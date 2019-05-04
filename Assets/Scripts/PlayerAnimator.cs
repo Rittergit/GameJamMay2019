6140a59197aac8fd78414acaab17a8eba78cd4d0
Daniel Martin <daniel.martin@hicknhack-software.com>
2019-05-04 19:10:09 +0200

    Added syncronizing of player animations

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerAnimator : NetworkBehaviour
+{
+    public Animator playerAnimator;
+
+    void Update()
+    {
+        check();
+    }
+
+    void check()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        //Animation
+        playerAnimator.SetBool("moving", horizontal != 0 || vertical != 0);
+    }
+}
