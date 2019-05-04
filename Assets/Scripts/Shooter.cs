9c018f51d0c7dab9c95bc139bb96ba6c509ebbde
Nhat Linh <nhatlinhtr95@gmail.com>
2019-05-04 23:25:09 +0200

    update Shooter Script, + adding grass models + reorganize Art folder

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,16 +1,34 @@
 ﻿using UnityEngine;
 
 public class Shooter : MonoBehaviour
-{
+{   
+    [Tooltip("Time delay between to shoot. 1.0f = 1 seconds")]
+    public float shootDelay = 1f;
+    public AudioClip shootSound;
+    public Transform shootPoint;
     [SerializeField] private Animator animator;
+    private float timer;
 
     void Update()
     {
-        var shooting = Input.GetButtonDown("Fire");
-        Debug.Log("Shooting!");
-        if (shooting)
+        //Timer is running. Tick tick .... tok
+        timer += Time.deltaTime;
+
+        if(timer >= shootDelay && Input.GetButtonDown("Fire"))
         {
+            Debug.Log("Choo choo choo, Mother fucker!");
+
+            //Set Trigger Animation
             this.animator.SetTrigger("shoot");
+
+            //FX
+            if (shootSound)
+            {
+                //Play Sound Effect here
+            }
+
+            //reset Timer
+            timer = 0f;
         }
     }
 }
