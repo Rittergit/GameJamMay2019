b129392493d3990a763ced642f532a1087708b9c
Nhat Linh <nhatlinhtr95@gmail.com>
2019-05-04 17:28:55 +0200

    Add Anination in PlayerMovement Script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,23 +3,38 @@
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Attributes")]
     public float speed = 3f;
 
     private Rigidbody rb;
+    private Animator animator;
 
     void Start()
     {
         this.rb = this.GetComponent<Rigidbody>();
+        this.animator = this.GetComponentInChildren<Animator>();
     }
 
-    void Update()
+    void FixedUpdate()
+    {
+        CharacterMovement();
+    }
+
+    //MOVEMENT
+    void CharacterMovement()
     {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
         var direction = new Vector3(horizontal, 0f, vertical);
-        this.rb.rotation = Quaternion.LookRotation(direction);
+        if (direction.magnitude > 0)
+        {
+            this.rb.rotation = Quaternion.LookRotation(direction);
+        }
 
         var movement = Vector3.ClampMagnitude(direction, 1f) * this.speed;
         this.rb.velocity = movement;
+
+        //Animation
+        animator.SetBool("moving", horizontal != 0 || vertical != 0);
     }
 }
