2b220fb85553651296e9bc52763091af81c5c45a
Braden Walters <vc@braden-walters.info>
2019-05-04 17:44:01 +0200

    * Shoot script. * Update input axes.

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    void Update()
+    {
+        var shooting = Input.GetButtonDown("Fire");
+        Debug.Log("Shooting!");
+        this.animator.SetBool("shooting", shooting);
+    }
+}
