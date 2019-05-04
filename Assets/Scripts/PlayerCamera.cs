863d920f53f18a99672d340cb6f0d86e069f288a
Braden Walters <vc@braden-walters.info>
2019-05-04 13:51:16 +0200

    Corrected values for player camera

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -9,9 +9,9 @@ public class PlayerCamera : MonoBehaviour
     void Update()
     {
         var position = this.player.position;
-        this.transform.position = position + new Vector3(0f, 5f, -5f);
+        this.transform.position = position + new Vector3(0f, 5f, -6f);
 
         var rotation = this.player.rotation;
-        this.transform.rotation = rotation * Quaternion.Euler(45f, 0f, 0f);
+        this.transform.rotation = rotation * Quaternion.Euler(35f, 0f, 0f);
     }
 }
