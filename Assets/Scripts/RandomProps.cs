e8aab2095b9b8c37239d47dd0bde1648b043944a
Nhat Linh <nhatlinhtr95@gmail.com>
2019-05-05 14:59:54 +0200

    Sounds update

--- Assets/Scripts/RandomProps.cs
+++ Assets/Scripts/RandomProps.cs
@@ -1,18 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//using System.Collections;
+//using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomProps : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Settings")]
+    public GameObject objectToSpawn;
+    public Transform[] spawnPoints;
+    [Tooltip("Where it should spawn")]
+    public Transform parent;
+    public bool canScale;
+    public bool canRotate;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        if (spawnPoints.Length <= 0) return;
+
+        foreach(Transform point in spawnPoints)
+        {
+            GameObject newGO = Instantiate(objectToSpawn, point.position, point.rotation);
+
+            if (canScale)
+            {
+                float newScale = Random.Range(0.3f, 1f);
+                newGO.transform.localScale -= new Vector3(newScale, newScale, newScale);
+            }
+
+            if (canRotate)
+            {
+                newGO.transform.Rotate(0, Random.Range(0f, 180f), 0, Space.Self);
+            }
+
+            newGO.transform.parent = parent.transform;
+        }
+
+        Destroy(this, 1f);
     }
 }
