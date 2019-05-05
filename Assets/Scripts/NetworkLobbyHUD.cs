3dd83ee220e8aeb7a8f1fae10816eebfd12f34e5
Daniel Martin <daniel.martin@hicknhack-software.com>
2019-05-05 14:28:39 +0200

    Added network lobby hud

---
+++ Assets/Scripts/NetworkLobbyHUD.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkLobbyHUD : MonoBehaviour
+{
+    public string IpAddress;
+    public string Port;
+    private bool connected = false;
+    private bool showDisconnect = false;
+
+    void Update()
+    {
+        if (connected && Input.GetButtonUp("Pause"))
+        {
+            showDisconnect = !showDisconnect;
+        }
+    }
+
+    public void OnGUI()
+    {
+        if (!connected)
+        {
+            if (GUILayout.Button("Host"))
+            {
+                connected = true;
+                NetworkLobbyManager.singleton.networkPort = int.Parse(Port);
+                NetworkLobbyManager.singleton.StartHost();
+            }
+
+            IpAddress = GUILayout.TextField(IpAddress, GUILayout.Width(100));
+            Port = GUILayout.TextField(Port, 5);
+            if (GUILayout.Button("Connect"))
+            {
+                connected = true;
+                NetworkLobbyManager.singleton.networkAddress = IpAddress;
+                NetworkLobbyManager.singleton.networkPort = int.Parse(Port);
+                NetworkLobbyManager.singleton.StartClient();
+            }
+        }
+        else
+        {
+            if (showDisconnect)
+            {
+                if (GUILayout.Button("Disconnect"))
+                {
+                    connected = false;
+                    NetworkLobbyManager.singleton.StopHost();
+                }
+            }
+        }
+    }
+}
