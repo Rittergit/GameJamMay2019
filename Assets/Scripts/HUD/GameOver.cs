47be90fffa588ea23ad98fe0a1bebc42d11ac96a
Braden Walters <vc@braden-walters.info>
2019-05-05 17:21:47 +0200

    * Show appropriate win/lose screen.

--- Assets/Scripts/HUD/GameOver.cs
+++ Assets/Scripts/HUD/GameOver.cs
@@ -5,6 +5,8 @@ using UnityEngine.Networking;
 public class GameOver : MonoBehaviour
 {
     [SerializeField] private GameObject window;
+    [SerializeField] private GameObject slaveWin;
+    [SerializeField] private GameObject landlordWin;
 
     private float restoreTimeScale;
 
@@ -23,13 +25,22 @@ public class GameOver : MonoBehaviour
 
     public void Exit()
     {
+        Time.timeScale = this.restoreTimeScale;
+
         // TODO: Exit to lobby.
         NetworkManager.singleton.StopServer();
     }
 
     private void OnGameOver(object sender, EventArgs e)
     {
+        var gm = GameManager.Singleton;
+
         this.window.SetActive(true);
+        this.slaveWin.SetActive(
+            gm.CurrentWinner == GameManager.WinnerType.Slave);
+        this.landlordWin.SetActive(
+            gm.CurrentWinner == GameManager.WinnerType.Landlord);
+
         this.restoreTimeScale = Time.timeScale;
         Time.timeScale = 0f;
     }
