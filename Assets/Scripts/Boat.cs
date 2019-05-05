7b4bf08b0d5e1f88abc02d036732572adbd805a7
Daniel Martin <daniel.martin@hicknhack-software.com>
2019-05-05 19:02:06 +0200

    Added some things

--- Assets/Scripts/Boat.cs
+++ Assets/Scripts/Boat.cs
@@ -1,25 +1,8 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 
 public class Boat : MonoBehaviour
 {
-    void Start()
-    {
-        var allSpawnPoints =
-            GameObject.FindGameObjectsWithTag(Tags.BoatSpawnPoint);
-
-        var random = new System.Random();
-        var spawnPoint = allSpawnPoints.Length > 0
-            ? allSpawnPoints[random.Next() % allSpawnPoints.Length]
-            : null;
-
-        Debug.Assert(spawnPoint != null, "No spawn points for boat");
-        if (spawnPoint != null)
-        {
-            this.transform.position = spawnPoint.transform.position;
-            this.transform.rotation = spawnPoint.transform.rotation;
-        }
-    }
-
     void OnTriggerEnter(Collider other)
     {
         var player = other.gameObject.GetComponent<PlayerSpawn>();
---
+++ Assets/Scripts/BoatSpawn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class BoatSpawn : MonoBehaviour
+{
+    public GameObject boatPrefab;
+
+    void Start()
+    {
+        var allSpawnPoints =
+            GameObject.FindGameObjectsWithTag(Tags.BoatSpawnPoint);
+
+        var random = new System.Random();
+        var spawnPoint = allSpawnPoints.Length > 0
+            ? allSpawnPoints[random.Next() % allSpawnPoints.Length]
+            : null;
+
+        Debug.Assert(spawnPoint != null, "No spawn points for boat");
+        if (spawnPoint != null)
+        {
+            GameObject boat = Instantiate(boatPrefab, spawnPoint.transform.position, spawnPoint.transform.rotation);
+
+            NetworkServer.Spawn(boat);
+        }
+    }
+}
