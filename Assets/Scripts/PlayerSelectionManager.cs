71c8962006f35f0b14923b62123b247ff7638969
Braden Walters <vc@braden-walters.info>
2019-05-05 12:02:45 +0200

    * Custom network manager

---
+++ Assets/Scripts/PlayerSelectionManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSelectionManager : NetworkLobbyManager
+{
+    [SerializeField] private GameObject[] playerTypes;
+
+    private int nextPlayerType;
+
+    public override void OnServerAddPlayer(
+        NetworkConnection conn,
+        short playerControllerId)
+    {
+        Debug.Assert(this.nextPlayerType < this.playerTypes.Length);
+        var playerType = this.playerTypes[this.nextPlayerType++];
+        var player = Instantiate(playerType);
+        NetworkServer.AddPlayerForConnection(
+            conn,
+            player,
+            playerControllerId);
+    }
+}
