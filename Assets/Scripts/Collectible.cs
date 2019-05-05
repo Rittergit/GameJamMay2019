e29895605d5b978d8aa79b6b023992de63af79f3
Braden Walters <vc@braden-walters.info>
2019-05-05 17:05:15 +0200

    * Collectibles.

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    public enum CollectibleType
+    {
+        Paddle,
+        Food
+    }
+
+    [SerializeField] private CollectibleType type;
+
+    void OnTriggerEnter(Collider other)
+    {
+        var player = other.gameObject.GetComponent<PlayerSpawn>();
+        var isSlave = player != null
+            && player.Type == PlayerSpawn.PlayerType.Slave;
+        if (isSlave)
+        {
+            switch (this.type)
+            {
+                case CollectibleType.Paddle:
+                    GameManager.Singleton.CollectPaddle();
+                    break;
+
+                case CollectibleType.Food:
+                    GameManager.Singleton.CollectFood();
+                    break;
+            }
+
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
