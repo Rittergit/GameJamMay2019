e77c90a5e8e74382488877e5e4ac087a54a0dc35
Braden Walters <vc@braden-walters.info>
2019-05-12 11:05:57 +0200

    * Disclaimer about game content

---
+++ Assets/Scripts/Disclaimer/ContinueButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ContinueButton : MonoBehaviour
+{
+    [SerializeField] private SceneAsset nextScene;
+
+    public void GoToNextScene()
+    {
+        SceneManager.LoadScene(this.nextScene.name);
+    }
+}
